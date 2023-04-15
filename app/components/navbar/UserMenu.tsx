ff552ea0711593c5ae32b68e11ed985a1082dcd5
Zubair Abubakar <zuby52@gmail.com>
2023-04-15 16:08:27 +0100

    added properties page

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -75,7 +75,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                                     label="My Reservations"
                                 />
                                 <MenuItem
-                                    onClick={() => {}}
+                                    onClick={() => router.push('/properties')}
                                     label="My Properties"
                                 />
                                 <MenuItem
